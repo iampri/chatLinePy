c18b4264a3b46ff4ada5e0393756b04cdbdd629e
PriewMAXIM <priewmaxim@gmail.com>
2017-06-11 21:02:09 +0700

    logic3

--- callback/views.py
+++ callback/views.py
@@ -67,6 +67,12 @@ def index(request):
         if not isinstance(event.message, TextMessage):
             continue
 
+        if 'userId' in event.source:
+            logger.info("userId " +event.source.userId)
+            
+        if 'groupId' in event.source:
+            logger.info("groupId " + event.source.groupId)
+            
         logger.info(event.message.text)
             
         if event.message.text.upper() == 'M':
