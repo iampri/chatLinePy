f1a83a94c4f17edf8aa18628dfd8bc91fdec034c
PriewMAXIM <priewmaxim@gmail.com>
2017-06-11 18:45:33 +0700

    update2

--- callback/views.py
+++ callback/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseNotAllowed, HttpResponseBadRequest
