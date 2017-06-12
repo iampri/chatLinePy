c867aa3392a32f30f5ac0eb7abc915d7c0abc290
PriewMAXIM <priewmaxim@gmail.com>
2017-06-12 21:18:08 +0700

    more

---
+++ pgpull.bat
@@ -0,0 +1,5 @@
+set PGUSER=botsteelframe
+
+set PGPASSWORD=B1990otsteelframe
+
+heroku pg:pull DATABASE_URL botsteelframe  --app obscure-bastion-79958
---
+++ pgpush.bat
@@ -0,0 +1,9 @@
+set PGUSER=botsteelframe
+
+set PGPASSWORD=B1990otsteelframe
+
+rem heroku pg:push botsteelframe postgresql-regular-62062 --app obscure-bastion-79958
+
+rem heroku pg:push botsteelframe DATABASE_URL --app obscure-bastion-79958
+
+heroku pg:push botsteelframe DATABASE_URL --app obscure-bastion-79958
