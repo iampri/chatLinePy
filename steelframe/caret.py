e6466ade64050fb8e538f028d483d12c0e25a776
PriewMAXIM <priewmaxim@gmail.com>
2017-06-15 09:56:09 +0700

    fix12

--- steelframe/caret.py
+++ steelframe/caret.py
@@ -30,4 +30,4 @@ class Caret():
         prizef3 = tree.xpath('//div[@id="prize-f3"]/div/span/span/text()')
         prizel3 = tree.xpath('//div[@id="prize-l3"]/div/span/span/text()')
         prizen1 = tree.xpath('//div[@id="prize-n1"]/div/span/span/text()')
-        return 'งวดวันที่ '+ period + ", รางวัลที่ 1 " + prize1[0] + ', เลขท้าย 2 ตัว ' + prizel2[0] + ', เลขหน้า 3 ตัว ' + prizef3[0] + ', เลขท้าย 3 ตัว ' + prizel3[0] + ', รางวัลข้างเคียงรางวัลที่ 1 ' + prizen1[0]
+        return 'งวดวันที่ '+ period[0] + ", รางวัลที่ 1 " + prize1[0] + ', เลขท้าย 2 ตัว ' + prizel2[0] + ', เลขหน้า 3 ตัว ' + prizef3[0] + ', เลขท้าย 3 ตัว ' + prizel3[0] + ', รางวัลข้างเคียงรางวัลที่ 1 ' + prizen1[0]
